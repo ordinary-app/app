ed84604c1534fbf709258aad046384cf7efb35ad
segment7 <eszxcde@hotmail.com>
2025-08-20 14:26:22 +0800

    feat: explorer view

--- src/stores/app-config-store.ts
+++ src/stores/app-config-store.ts
@@ -7,5 +7,5 @@ type AppConfigState = {
 
 export const useAppConfigStore = create<AppConfigState>((set) => ({
   contractAddress: "0x13036d898B64663A669f9eBb707805c57F8B1b56",
-  explorerUrl: "https://sepolia.etherscan.io/tx/",
+  explorerUrl: "https://sepolia.etherscan.io/",
 }));
